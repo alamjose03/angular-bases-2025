97199f76f69c1fb9edf9b505de9b8e59a8ecd373
Alam Perez Cih <alam.perez@blueoceantech.com.mx>
2025-12-11 11:11:23 -0600

    Fin de sección 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  public counter: number = 10;
+
+  constructor() { }
+
+  public increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  public decreaseBy(value: number): void {
+    this.counter -= value;
+  }
+
+  public reset(): void {
+    this.counter = 10;
+  }
+}
